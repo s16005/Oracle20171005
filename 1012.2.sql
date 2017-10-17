30fff7c0763dff79b6c94a5d0652dd8322fddbe4
s16005 <s16005@std.it-college.ac.jp>
2017-10-17 13:50:02 +0900

    commit

--- 1012.2.sql
+++ 1012.2.sql
@@ -3,6 +3,6 @@ select employee_id, job_id "職務", case job_id when 'AD_PRES' then 'A'
                                         when 'IT_PROG' then 'C'
                                         when 'SA_REP' then 'D'
                                         when 'ST_CLERK' then 'E'
-                                        else '0' "等級"
-                            end
+                                        else '0'
+                                   end "等級"
 from employees;
