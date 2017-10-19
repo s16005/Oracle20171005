9254b07cbea1d57ecf4051de93d2528290d91aa4
s16005 <s16005@std.it-college.ac.jp>
2017-10-19 17:48:25 +0900

    commit

--- 1019.2.sql
+++ 1019.2.sql
@@ -1,7 +1,7 @@
-select department_id, to_char(null)
+select department_id
 from employees
 where department_id is not null
 intersect
-select department_id, to_char(null)
+select department_id
 from employees
 where job_id != 'ST_CREAK' and department_id is not null;
