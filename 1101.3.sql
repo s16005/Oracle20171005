5fc6959446ceff85357c1c2987cf37afadbe28af
s16005 <s16005@std.it-college.ac.jp>
2017-11-02 14:01:39 +0900

    1011.3.sqlを修正

--- 1101.3.sql
+++ 1101.3.sql
@@ -1,4 +1,4 @@
-select last_name, round(months_between(sysdate, hire_date) / 12) "years", round(months_between(sysdate, hire_date)) "months"
+select last_name, round(months_between(sysdate, hire_date) / 12) "years" ,round((mod(months_between(sysdate, hire_date), 12))) "months"
 from employees
 order by round(months_between(sysdate, hire_date) / 12) desc
 fetch first 10 rows only;
