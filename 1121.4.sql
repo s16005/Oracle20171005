0f1ad47076fb37ef241e63d974768f899a66ff69
s16005 <s16005@std.it-college.ac.jp>
2017-11-21 16:50:19 +0900

    commit

---
+++ 1121.4.sql
@@ -0,0 +1,3 @@
+select last_name || 'is a' || job_id "Employee Details"
+from employees
+order by employee_id;
---
+++ 1121.5.sql
@@ -0,0 +1,4 @@
+select job_id "Job"
+from employees
+group by job_id
+order by job_id;
